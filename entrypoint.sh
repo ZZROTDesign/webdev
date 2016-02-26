ef25390af7bc8204e9218904f0f34d138271d595
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2016-02-26 15:15:17 -0500

    Added update commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ npm install -g gulp
 #Install all packages from Package.json.
 npm install
 
+#For updating
+#npm install -g npm-check-updates
+#npm-check-updates -u
+
 #Run our default Gulp task.
 gulp
